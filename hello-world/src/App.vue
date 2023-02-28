c2e028b103f8a5e8d1f641b789a6f6a2a3341316
Justin Douville <jdouville8@gmail.com>
2023-02-27 22:46:27 -0700

    <Intialize application and add logo>

---
+++ hello-world/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/backleft.jpg">
+  <HelloWorld msg="BackLeft Vue JS App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
